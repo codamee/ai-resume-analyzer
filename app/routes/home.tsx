dc3e8e4a37191959e0d23d02355d557c9999c033
naveen <185893220+codamee@users.noreply.github.com>
2026-03-29 11:45:16 +0530

    feat : add interface and utilities for ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,28 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeX" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')]  bg-cover">
+    <Navbar></Navbar>
+    <section className="main-section">
+      <div>
+        <h1>Track your applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-Powered feedback</h2>
+      </div>
+    </section>
+
+    {
+      resumes.map((resume) => {
+        return <div>{resume.jobTitle}</div>
+      })
+    }
+  </main>;
 }
